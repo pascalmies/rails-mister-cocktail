d5a7922b625a7c41e6bcce48aa29ab02c15b7179
Pascal Mies <pascalniklasmies@gmail.com>
2018-08-09 15:37:14 +0200

    functions is implemented and running

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,38 @@
+class DosesController < ApplicationController
+
+  def index
+    @doses = Doses.all
+  end
+
+  def show
+    @dose = Dose.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(doses_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail.id)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail_id)
+  end
+
+  private
+
+  def doses_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+  end
+end
